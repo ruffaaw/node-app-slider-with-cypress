459d95ab5514affe96787a8a67faf987344dfa56
ruffaaw <rafal.p.ciupek@gmail.com>
2024-03-21 15:29:24 +0100

    Added sth

--- cypress/e2e/slider.cy.js
+++ cypress/e2e/slider.cy.js
@@ -1,6 +1,6 @@
 describe('Swiper Gallery Test', function () {
   it('Checks if second slide contains "United Kingdom"', function () {
-    cy.visit('http://app:3000');
+    cy.visit('http://localhost:3000');
     cy.get('.swiper-button-next').click();
     cy.get('.swiper-slide-active').should('contain', 'United Kingdom');
   });
@@ -8,11 +8,22 @@ describe('Swiper Gallery Test', function () {
 
 describe('Swiper Gallery Test', function () {
   it('Checks if third slide contains "Paris"', function () {
-    cy.visit('http://app:3000');
+    cy.visit('http://localhost:3000');
     cy.get('.swiper-button-next').click();
     cy.wait(2000)
     cy.get('.swiper-button-next').click({ force: true });
     cy.wait(2000)
     cy.get('.swiper-slide-active').should('contain', 'Paris');
+  });
+});
+
+describe('Swiper Gallery Test', function () {
+  it('Checks if preview slide contains "Italy"', function () {
+    cy.visit('http://localhost:3000');
+    cy.get('.swiper-button-next').click();
+    cy.wait(2000)
+    cy.get('.swiper-button-prev').click({ force: true });
+    cy.wait(2000)
+    cy.get('.swiper-slide-active').should('contain', 'Italy');
   });
 });
